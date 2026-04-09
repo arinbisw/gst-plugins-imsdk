9b7d16d0978732bee947002f6ae8d4182de7f0ca
Arindam Biswas <arinbisw@qti.qualcomm.com>
2026-04-09 16:44:29 +0530

    Adding folder restructuring
    Signed-off-by: Arindam Biswas <arinbisw@qti.qualcomm.com>

---
+++ examples/python/gst-camera-opencv-resize.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+################################################################################
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+################################################################################
+
+from gi.repository import Gst, GLib
+import os
+import sys
+import signal
+import argparse
+import cv2
+import time
+
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version("GLib", "2.0")
+
+# Constants
+DESCRIPTION = """
+This application demonstrate live camera stream using opencv api's which include color conversion and resize.
+App capture camera frame as an input using cv videocapture and convert to RGBA using downstream plugin
+and resize to user specified resolution. Resize output will display on screen.
+
+To enable Qt applications to run using the Wayland display server with EGL rendering.
+Use mentioned below recommended Environment Variable:
+
+export QT_QPA_PLATFORM=wayland-egl
+
+usecase:
+gst-camera-opencv-resize.py --inwidth 1280 --inheight 720 --outwidth 640 --outheight 480
+"""
+
+DEFAULT_INPUT_WIDTH = 1280
+DEFAULT_INPUT_HEIGHT = 720
+DEFAULT_RESIZE_WIDTH = 640
+DEFAULT_RESIZE_HEIGHT = 480
+
+
+def read_cam():
+    print(cv2.getBuildInformation())
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=type(
+            'CustomFormatter',
+            (argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter),
+            {}
+        )
+    )
+    parser.add_argument(
+        "--inwidth", type=int, default=DEFAULT_INPUT_WIDTH,
+        help="Input camera stream width"
+    )
+    parser.add_argument(
+        "--inheight", type=int, default=DEFAULT_INPUT_HEIGHT,
+        help="Input camera stream height"
+    )
+    parser.add_argument(
+        "--outwidth", type=int, default=DEFAULT_RESIZE_WIDTH,
+        help="Resize camera stream width"
+    )
+    parser.add_argument(
+        "--outheight", type=int, default=DEFAULT_RESIZE_HEIGHT,
+        help="Resize camera stream height"
+    )
+    args = parser.parse_args()
+    print("Input and Output resolution: ", args.inwidth,
+          args.inheight, args.outwidth, args.outheight)
+
+    source_path = (
+        f"qtiqmmfsrc name=camsrc video_0::type=video ! "
+        f"video/x-raw,format=NV12,width={args.inwidth},height={args.inheight},framerate=30/1 ! "
+        "queue ! "
+        "qtivtransform ! "
+        "video/x-raw,format=RGBA ! "
+        "appsink"
+    )
+
+    cap = cv2.VideoCapture(source_path)
+
+    if cap.isOpened():
+        print("Capture API success")
+
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        print("Caps properties: ", width, height, fps)
+
+        try:
+            while True:
+                ret_val, frame = cap.read()
+                if not ret_val:
+                    break
+                resize = cv2.resize(frame, (args.outwidth, args.outheight))
+                cv2.imshow('demo', resize)
+                cv2.waitKey(1)
+        except KeyboardInterrupt:
+            print("\nInterrupted by user. Exiting gracefully...")
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Capture API failed")
+
+def is_linux():
+    try:
+        with open("/etc/os-release") as f:
+            for line in f:
+                if "Linux" in line:
+                    return True
+    except FileNotFoundError:
+        return False
+    return False
+
+def main():
+    """Main function to set up and run the OpenCV with color conversion and resize  plugins."""
+
+    # Set the environment
+    if is_linux():
+        os.environ["XDG_RUNTIME_DIR"] = "/dev/socket/weston"
+        os.environ["WAYLAND_DISPLAY"] = "wayland-1"
+
+    read_cam()
+    print("App execution successful")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
